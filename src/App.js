db67cfd5b2ebc96531cee05e166218b60db70a19
Ian Isom <ian.isom98@gmail.com>
2021-05-01 11:14:58 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          You are Awesome
+          You are Awesome!
         </p>
         <a
           className="App-link"
